f4e273ac1913724247ad57763130e61ce8f6530b
JTB Development <jtbdevelopment@gmail.com>
2014-10-25 19:40:06 -0400

    Initial commit

---
+++ model/src/main/java/com/jtbdevelopment/TwistedHangman/model/BasicHangmanGame.groovy
@@ -0,0 +1,110 @@
+package com.jtbdevelopment.TwistedHangman.model
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.util.StringUtil
+import org.springframework.util.StringUtils
+
+/**
+ * Date: 10/25/2014
+ * Time: 5:20 PM
+ */
+@CompileStatic
+class BasicHangmanGame implements Serializable {
+    final int guessesUntilHung;
+    final char[] wordPhrase;
+    final char[] displayedWordPhrase;
+    final String category;
+    private final SortedSet<Character> distinctLetters = new TreeSet<>();
+    private final SortedSet<Character> goodGuessedLetters = new TreeSet<>();
+    private final SortedSet<Character> badGuessedLetters = new TreeSet<>();
+    private final SortedSet<Character> guessedLetters = new TreeSet<>();
+
+    public BasicHangmanGame(final String wordPhraseAsString, final String category, int guessesUntilHung) {
+        if(guessesUntilHung <= 0) {
+            throw new IllegalArgumentException("max guesses must be positive number")
+        }
+        if(StringUtils.isEmpty(wordPhraseAsString)) {
+            throw new IllegalArgumentException("invalid wordPhrase")
+        }
+        if(StringUtils.isEmpty(category)) {
+            throw new IllegalArgumentException("category is invalid")
+        }
+        this.category = category.toUpperCase()
+        this.guessesUntilHung = guessesUntilHung ?: 0
+        this.wordPhrase = wordPhraseAsString.toUpperCase().toCharArray()
+        displayedWordPhrase = new char[wordPhrase.length]
+
+        for (int i = 0; i < wordPhrase.length; ++i) {
+            if (Character.isLetter(wordPhrase[i])) {
+                distinctLetters.add(wordPhrase[i]);
+                displayedWordPhrase[i] = '_';
+            } else {
+                displayedWordPhrase[i] = wordPhrase[i];
+            }
+        }
+    }
+
+    public int getHangingParts() {
+        return badGuessedLetters.size();
+    }
+
+    public boolean isGameWon() {
+        return distinctLetters.size() == goodGuessedLetters.size();
+    }
+
+    public boolean isGameLost() {
+        return getHangingParts() == guessesUntilHung;
+    }
+
+    public boolean isGameOver() {
+        return (isGameLost() || isGameWon());
+    }
+
+    public String getWordPhraseAsString() {
+        return new String(wordPhrase);
+    }
+
+    public String getDisplayedWordPhraseAsString() {
+        return new String(displayedWordPhrase);
+    }
+
+    public int guessLetter(final Character letter) {
+        if(!letter.isLetter()) {
+            throw new IllegalArgumentException("Non alphabetic guess");
+        }
+
+        char uppercaseLetter = letter.toUpperCase();
+        if(guessedLetters.contains(uppercaseLetter)) {
+            throw new IllegalArgumentException("Already guessed this letter")
+        }
+
+        int found = 0;
+        for (int i = 0; i < wordPhrase.length; ++i) {
+            if (wordPhrase[i] == uppercaseLetter) {
+                displayedWordPhrase[i] = uppercaseLetter;
+                ++found;
+            }
+        }
+
+        if (found > 0) {
+            goodGuessedLetters.add(uppercaseLetter);
+        } else {
+            badGuessedLetters.add(uppercaseLetter);
+        }
+        guessedLetters.add(uppercaseLetter);
+
+        return found;
+    }
+
+    public SortedSet<Character> getGoodGuessedLetters() {
+        return Collections.unmodifiableSortedSet(goodGuessedLetters);
+    }
+
+    public SortedSet<Character> getBadGuessedLetters() {
+        return Collections.unmodifiableSortedSet(badGuessedLetters);
+    }
+
+    public SortedSet<Character> getGuessedLetters() {
+        return Collections.unmodifiableSortedSet(guessedLetters);
+    }
+}
---
+++ model/src/test/java/com/jtbdevelopment/TwistedHangman/model/BasicHangmanGameTest.groovy
@@ -0,0 +1,129 @@
+package com.jtbdevelopment.TwistedHangman.model
+
+import org.junit.Test
+
+/**
+ * Date: 10/25/2014
+ * Time: 6:28 PM
+ */
+class BasicHangmanGameTest extends GroovyTestCase {
+    @Test
+    public void testInitialGameStateForBasicWord() {
+        BasicHangmanGame game = new BasicHangmanGame("cat", "animal", 5)
+        assert !game.gameOver
+        assert !game.gameLost
+        assert !game.gameWon
+        assert game.badGuessedLetters.empty
+        assert game.goodGuessedLetters.empty
+        assert game.guessedLetters.empty
+        assert game.displayedWordPhrase == ['_', '_', '_']
+        assert game.displayedWordPhraseAsString == "___"
+        assert game.wordPhrase == ['C', 'A', 'T']
+        assert game.wordPhraseAsString == "CAT"
+        assert game.guessesUntilHung == 5
+        assert game.hangingParts == 0
+        assert game.category == "ANIMAL"
+    }
+
+    @Test
+    public void testInitialGameStateForPhraseWithNonAlphabeticCharacters() {
+        BasicHangmanGame game = new BasicHangmanGame("It's a wonderful life.", "Movie", 7)
+        assert game.displayedWordPhraseAsString == "__'_ _ _________ ____."
+        assert game.wordPhraseAsString == "IT'S A WONDERFUL LIFE."
+        assert game.guessesUntilHung == 7
+        assert game.category == "MOVIE"
+    }
+
+    @Test
+    public void testSingleLetterMatchGuess() {
+        BasicHangmanGame game = new BasicHangmanGame("Find A Letter", "Junk", 10)
+        assert game.guessLetter((char) 'i') == 1;
+        assert !game.gameOver
+        assert game.badGuessedLetters.empty
+        assert game.guessedLetters == new TreeSet(['I'])
+        assert game.goodGuessedLetters == new TreeSet(['I'])
+        assert game.displayedWordPhraseAsString == "_I__ _ ______"
+        assert game.category == "JUNK"
+    }
+
+    @Test
+    public void testMultipleLetterMatchGuess() {
+        BasicHangmanGame game = new BasicHangmanGame("Find A Letter", "JUNK", 10)
+        assert game.guessLetter((char) 'I') == 1;
+        assert game.guessLetter((char) 'e') == 2;
+        assert !game.gameOver
+        assert game.hangingParts == 0
+        assert game.badGuessedLetters.empty
+        assert game.guessedLetters == new TreeSet(['E', 'I'])
+        assert game.goodGuessedLetters == new TreeSet(['E', 'I'])
+        assert game.displayedWordPhraseAsString == "_I__ _ _E__E_"
+    }
+
+    @Test
+    public void testLetterMatchGuessFailure() {
+        BasicHangmanGame game = new BasicHangmanGame("Find A Letter", "JUNK", 10)
+        assert game.guessLetter((char) 'I') == 1;
+        assert game.guessLetter((char) 'e') == 2;
+        assert game.guessLetter((char) 'Z') == 0;
+        assert game.guessLetter((char) 'X') == 0;
+        assert !game.gameOver
+        assert game.hangingParts == 2
+        assert game.badGuessedLetters == new TreeSet(['X', 'Z'])
+        assert game.guessedLetters == new TreeSet(['E', 'I', 'X', 'Z'])
+        assert game.goodGuessedLetters == new TreeSet(['E', 'I'])
+        assert game.displayedWordPhraseAsString == "_I__ _ _E__E_"
+    }
+
+    @Test
+    public void testGameLost() {
+        BasicHangmanGame game = setUpAlmostFinishedGame()
+
+        game.guessLetter((char) 'T')
+        assert !game.gameWon
+        assert game.gameLost
+        assert game.gameOver
+
+        assert game.hangingParts == game.guessesUntilHung
+        assert game.displayedWordPhraseAsString == "WI__ER"
+        assert game.badGuessedLetters == new TreeSet(['G', 'X', 'L', 'M', 'T'])
+        assert game.goodGuessedLetters == new TreeSet(['W', 'I', 'E', 'R'])
+        assert game.guessedLetters == new TreeSet(['W', 'I', 'E', 'R', 'G', 'X', 'L', 'M', 'T'])
+    }
+
+    @Test
+    public void testGameWon() {
+        BasicHangmanGame game = setUpAlmostFinishedGame()
+
+        game.guessLetter((char) 'N')
+        assert game.gameWon
+        assert !game.gameLost
+        assert game.gameOver
+
+        assert game.hangingParts < game.guessesUntilHung
+        assert game.displayedWordPhraseAsString == "WINNER"
+        assert game.badGuessedLetters == new TreeSet(['G', 'X', 'L', 'M'])
+        assert game.goodGuessedLetters == new TreeSet(['W', 'I', 'N', 'E', 'R'])
+        assert game.guessedLetters == new TreeSet(['W', 'I', 'E', 'R', 'G', 'X', 'L', 'M', 'N'])
+    }
+
+    private BasicHangmanGame setUpAlmostFinishedGame() {
+        BasicHangmanGame game = new BasicHangmanGame("Winner", "Not a Loser", 5);
+        ['w', 'i', 'e', 'r'].each {
+            String guess ->
+                game.guessLetter(guess.charAt(0))
+                assert !game.gameWon
+                assert !game.gameLost
+                assert !game.gameOver
+        }
+        ['g', 'x', 'l', 'm'].each {
+            String guess ->
+                game.guessLetter(guess.charAt(0))
+                assert !game.gameWon
+                assert !game.gameLost
+                assert !game.gameOver
+        }
+        assert game.hangingParts == 4
+        assert game.displayedWordPhraseAsString == "WI__ER"
+        game
+    }
+}
