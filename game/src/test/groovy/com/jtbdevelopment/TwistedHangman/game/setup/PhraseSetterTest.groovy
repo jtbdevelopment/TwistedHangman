0806cd617ade211cfadf4e59634559ea9cd02d2b
JTB <jtbdevelopment@gmail.com>
2014-11-07 06:55:48 -0500

    tests

---
+++ game/src/test/groovy/com/jtbdevelopment/TwistedHangman/game/setup/PhraseSetterTest.groovy
@@ -0,0 +1,37 @@
+package com.jtbdevelopment.TwistedHangman.game.setup
+
+import com.jtbdevelopment.TwistedHangman.game.state.IndividualGameState
+import org.junit.Test
+
+/**
+ * Date: 11/7/14
+ * Time: 6:48 AM
+ */
+class PhraseSetterTest extends GroovyTestCase {
+    private static String inputWordPhrase = "Bohemian Rhapsody"
+    private static String inputCategory = "Song"
+    private static String expectedWordPhrase = inputWordPhrase.toUpperCase()
+    private static String expectedCategory = inputCategory.toUpperCase()
+
+    PhraseSetter phraseSetter = new PhraseSetter()
+
+    @Test
+    public void testPhraseSetting() {
+        IndividualGameState gameState = new IndividualGameState([] as Set)
+        boolean initCalled = false
+        phraseSetter.phraseFeatureInitializers = [
+                [initializeForPhrase: {
+                    IndividualGameState state ->
+                        //  Check here that everything is set before inits are called
+                        assert state == gameState
+                        assert state.category == expectedCategory
+                        assert state.wordPhraseString == expectedWordPhrase
+                        initCalled = true
+                }] as PhraseFeatureInitializer
+        ]
+
+        phraseSetter.setWordPhrase(gameState, inputWordPhrase, inputCategory)
+        assert initCalled
+    }
+
+}
