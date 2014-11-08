fe0ee3b250dd77827d8e68f588ccc1a01bb5831a
JTB Development <jtbdevelopment@gmail.com>
2014-11-08 13:24:20 -0500

    adding tests

--- game/src/test/groovy/com/jtbdevelopment/TwistedHangman/THGroovyTestCase.groovy
+++ game/src/test/groovy/com/jtbdevelopment/TwistedHangman/THGroovyTestCase.groovy
@@ -11,8 +11,10 @@ abstract class THGroovyTestCase extends GroovyTestCase {
     protected static final Player PTWO = makeSimplePlayer("2")
     protected static final Player PTHREE = makeSimplePlayer("3")
     protected static final Player PFOUR = makeSimplePlayer("4")
+    protected static final Player PINACTIVE1 = makeSimplePlayer("I1", true)
+    protected static final Player PINACTIVE2 = makeSimplePlayer("I2", true)
 
-    protected static Player makeSimplePlayer(final String id) {
-        return new Player(id: id, source: "MADEUP", displayName: id)
+    protected static Player makeSimplePlayer(final String id, final boolean disabled = false) {
+        return new Player(id: id, source: "MADEUP", displayName: id, disabled: disabled)
     }
 }
---
+++ game/src/test/groovy/com/jtbdevelopment/TwistedHangman/game/factory/gamevalidators/PlayersActiveGameValidatorTest.groovy
@@ -0,0 +1,47 @@
+package com.jtbdevelopment.TwistedHangman.game.factory.gamevalidators
+
+import com.jtbdevelopment.TwistedHangman.THGroovyTestCase
+import com.jtbdevelopment.TwistedHangman.dao.PlayerRepository
+import com.jtbdevelopment.TwistedHangman.game.state.Game
+import org.junit.Test
+
+/**
+ * Date: 11/8/2014
+ * Time: 1:11 PM
+ */
+class PlayersActiveGameValidatorTest extends THGroovyTestCase {
+    PlayersActiveGameValidator validator = new PlayersActiveGameValidator()
+
+    @Test
+    public void testPassesAllActives() {
+        Game game = new Game()
+        game.players = [PONE, PTWO]
+        validator.playerRepository = [
+                findAll: {
+                    Iterable<String> input ->
+                        assert input.collect { it } as Set == [PONE.id, PTWO.id] as Set
+                        return [PONE, PTWO]
+                },
+        ] as PlayerRepository
+        assert validator.validateGame(game)
+    }
+
+    @Test
+    public void testFailsAnInactive() {
+        Game game = new Game()
+        game.players = [PONE, PINACTIVE2]
+        validator.playerRepository = [
+                findAll: {
+                    Iterable<String> input ->
+                        assert input.collect { it } as Set == [PONE.id, PINACTIVE2.id] as Set
+                        return [PONE, PINACTIVE2]
+                },
+        ] as PlayerRepository
+        assertFalse validator.validateGame(game)
+    }
+
+    @Test
+    public void testErrorMessage() {
+        assert validator.errorMessage() == "Game contains inactive players."
+    }
+}
