d2d5af38f79e01b70d2f6442220cc2274e7b1f19
JTB Development <jtbdevelopment@gmail.com>
2014-11-09 20:03:12 -0500

    undid bad commit of systemidmaker

--- utilities/src/main/groovy/com/jtbdevelopment/TwistedHangman/players/SystemIdMaker.groovy
+++ utilities/src/main/groovy/com/jtbdevelopment/TwistedHangman/players/SystemIdMaker.groovy
@@ -1,9 +1,6 @@
 package com.jtbdevelopment.TwistedHangman.players
 
-import com.jtbdevelopment.TwistedHangman.dao.GameRepository
 import com.jtbdevelopment.TwistedHangman.dao.PlayerRepository
-import com.jtbdevelopment.TwistedHangman.game.state.Game
-import com.jtbdevelopment.TwistedHangman.game.state.GameFeature
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 
@@ -29,16 +26,6 @@ class SystemIdMaker {
             assert player.displayName == Player.SYSTEM_ID_DISPLAY_NAME
         }
 
-        Game game = new Game();
-        GameRepository gameRepository = ctx.getBean(GameRepository.class)
-        game = gameRepository.save(game)
-//        game.created.
-//        Thread.sleep(10000)
-        game.features.add(GameFeature.SingleWinner)
-        game.players.add(repository.findOne(Player.SYSTEM_ID_ID))
-        game = gameRepository.save(game)
-
-        game = gameRepository.findOne(game.id)
         println "Complete"
     }
 }
