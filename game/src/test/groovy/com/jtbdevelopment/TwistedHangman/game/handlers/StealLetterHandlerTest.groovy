584e88dda2b2ea4953abc316012d3365438d8de6
JTB <jtbdevelopment@gmail.com>
2014-11-10 21:37:20 -0500

    Steal letter handler

--- game/src/test/groovy/com/jtbdevelopment/TwistedHangman/game/handlers/StealLetterHandlerTest.groovy
+++ game/src/test/groovy/com/jtbdevelopment/TwistedHangman/game/handlers/StealLetterHandlerTest.groovy
@@ -3,7 +3,7 @@ package com.jtbdevelopment.TwistedHangman.game.handlers
 import com.jtbdevelopment.TwistedHangman.TwistedHangmanTestCase
 import com.jtbdevelopment.TwistedHangman.exceptions.GameIsNotInPlayModeException
 import com.jtbdevelopment.TwistedHangman.exceptions.PlayerNotSolvingAPuzzleException
-import com.jtbdevelopment.TwistedHangman.game.mechanics.HangmanGameActions
+import com.jtbdevelopment.TwistedHangman.game.mechanics.ThievingHangmanGameActions
 import com.jtbdevelopment.TwistedHangman.game.state.Game
 import com.jtbdevelopment.TwistedHangman.game.state.IndividualGameState
 import org.junit.Test
@@ -12,35 +12,35 @@ import org.junit.Test
  * Date: 11/10/14
  * Time: 9:23 PM
  */
-class GuessLetterHandlerTest extends TwistedHangmanTestCase {
-    GuessLetterHandler handler = new GuessLetterHandler()
+class StealLetterHandlerTest extends TwistedHangmanTestCase {
+    StealLetterHandler handler = new StealLetterHandler()
 
     @Test
     public void testHandleGoodMove() {
         Game game = new Game(gamePhase: Game.GamePhase.Playing)
-        char letter = 'e'
+        int spot = 1
         IndividualGameState state = new IndividualGameState(wordPhrase: "Test", category: "Test")
         game.solverStates = [(PONE): state]
         handler.gameActions = [
-                guessLetter: {
-                    IndividualGameState s, char c ->
-                        assert c == letter
+                stealLetter: {
+                    IndividualGameState s, int p ->
+                        assert p == spot
                         assert state.is(s)
                 }
-        ] as HangmanGameActions
+        ] as ThievingHangmanGameActions
 
-        handler.handleActionInternal(PONE, game, letter)
+        handler.handleActionInternal(PONE, game, spot)
     }
 
     @Test
     public void testHandleBadPlayer() {
         Game game = new Game(gamePhase: Game.GamePhase.Playing)
-        char letter = 'e'
+        int spot = 1
         IndividualGameState state = new IndividualGameState(wordPhrase: "Test", category: "Test")
         game.solverStates = [(PONE): state]
 
         try {
-            handler.handleActionInternal(PTHREE, game, letter)
+            handler.handleActionInternal(PTHREE, game, spot)
             fail()
         } catch (PlayerNotSolvingAPuzzleException e) {
 
@@ -50,12 +50,12 @@ class GuessLetterHandlerTest extends TwistedHangmanTestCase {
     @Test
     public void testHandleBadGame() {
         Game game = new Game(gamePhase: Game.GamePhase.Setup)
-        char letter = 'e'
+        int spot = 1
         IndividualGameState state = new IndividualGameState(wordPhrase: "Test", category: "Test")
         game.solverStates = [(PONE): state]
 
         try {
-            handler.handleActionInternal(PTHREE, game, letter)
+            handler.handleActionInternal(PTHREE, game, spot)
             fail()
         } catch (GameIsNotInPlayModeException e) {
 
