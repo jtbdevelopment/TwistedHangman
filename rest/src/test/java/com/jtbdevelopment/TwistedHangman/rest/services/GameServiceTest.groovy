5bc44cfc3d6cfed976315c96c594cf3ce61c3421
JTB <jtbdevelopment@gmail.com>
2014-11-13 06:50:24 -0500

    rest cleanup

---
+++ rest/src/test/java/com/jtbdevelopment/TwistedHangman/rest/services/GameServiceTest.groovy
@@ -0,0 +1,43 @@
+package com.jtbdevelopment.TwistedHangman.rest.services
+/**
+ * Date: 11/13/14
+ * Time: 6:45 AM
+ */
+/*
+class GameServiceTest extends JerseyTest {
+    GameService gameService = new GameService()
+
+    void testPing() {
+        assert GameService.PING_RESULT == gameService.ping()
+    }
+
+    void testCreateNewGame() {
+        gameService.createNewGame()
+    }
+
+    void testCreateRematch() {
+
+    }
+
+    void testRejectGame() {
+
+    }
+
+    void testAcceptGame() {
+
+    }
+
+    void testSetPuzzle() {
+
+    }
+
+    void testStealLetter() {
+
+    }
+
+    void testGuessLetter() {
+
+    }
+}
+
+*/
