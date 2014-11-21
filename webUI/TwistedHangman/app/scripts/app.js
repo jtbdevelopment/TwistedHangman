4a113ceef7b7627edebd7367f5a6b4515b2ea9d7
JTB <jtbdevelopment@gmail.com>
2014-11-20 22:58:53 -0500

    weeded out yeoman info from about and main

--- webUI/TwistedHangman/app/scripts/app.js
+++ webUI/TwistedHangman/app/scripts/app.js
@@ -23,6 +23,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/create', {
+        templateUrl: 'views/create.html',
+        controller: 'CreateCtrl'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
