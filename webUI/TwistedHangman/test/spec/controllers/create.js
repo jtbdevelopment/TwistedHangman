52be225cc364369f2fe0aca78a6b1c8e689196db
JTB <jtbdevelopment@gmail.com>
2014-11-22 22:02:47 -0500

    tests

--- webUI/TwistedHangman/test/spec/controllers/create.js
+++ webUI/TwistedHangman/test/spec/controllers/create.js
@@ -1,56 +1,72 @@
 'use strict';
 
-describe('Controller: SinglePlayerCtrl', function () {
+describe('Controller: CreateCtrl', function () {
   // load the controller's module
   beforeEach(module('twistedHangmanApp'));
 
-  var ctrl, scope;
+  var ctrl, scope, http;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     scope = $rootScope.$new();
-    ctrl = $controller('SinglePlayerCtrl', {
+    http = $httpBackend;
+    ctrl = $controller('CreateCtrl', {
       $scope: scope
     });
   }));
 
   it('initializes', function () {
+    expect(scope.url).toEqual('/api/player/MANUAL1/new');
+    expect(scope.thieving).toEqual('Thieving');
+    expect(scope.drawGallows).toEqual('');
+    expect(scope.drawFace).toEqual('');
+    expect(scope.gamePace).toEqual('');
+
+    expect(scope.playerCount).toEqual('SinglePlayer');
+    expect(scope.players).toEqual([]);
+    expect(scope.gamePace).toEqual('');
+    expect(scope.puzzleSetter).toEqual('SystemPuzzles');
+    expect(scope.winners).toEqual('SingleWinner');
+    expect(scope.h2hEnabled).toBe(false);
+    expect(scope.alternatingEnabled).toBe(false);
+    expect(scope.allFinishedEnabled).toBe(false);
+    expect(scope.turnBasedEnabled).toBe(false);
+  });
+
+  it('resets to single player', function () {
+    //  should not change
+    scope.thieving = '';
+    scope.drawGallows = 'DrawGallows';
+    scope.drawFace = 'DrawFace';
+
+    //  should change
+    scope.playerCount = 'X';
+    scope.gamePace = 'TurnBased';
+    scope.players = ['1'];
+    scope.puzzleSetter = '';
+    scope.winners = '';
+    scope.h2hEnabled = true;
+    scope.alternatingEnabled = true;
+    scope.alternatingEnabled = true;
+    scope.turnBasedEnabled = true;
+
+    scope.setSinglePlayer();
+
+    expect(scope.url).toEqual('/api/player/MANUAL1/new');
+    expect(scope.thieving).toEqual('');
+    expect(scope.drawGallows).toEqual('DrawGallows');
+    expect(scope.drawFace).toEqual('DrawFace');
+    expect(scope.gamePace).toEqual('');
+
+    expect(scope.playerCount).toEqual('SinglePlayer');
+    expect(scope.players).toEqual([]);
+    expect(scope.gamePace).toEqual('');
+    expect(scope.puzzleSetter).toEqual('SystemPuzzles');
+    expect(scope.winners).toEqual('SingleWinner');
+    expect(scope.h2hEnabled).toBe(false);
+    expect(scope.alternatingEnabled).toBe(false);
+    expect(scope.allFinishedEnabled).toBe(false);
+    expect(scope.turnBasedEnabled).toBe(false);
   });
 });
 
-describe('Controller: TwoPlayerCtrl', function () {
-  // load the controller's module
-  beforeEach(module('twistedHangmanApp'));
-
-  var ctrl, scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ctrl = $controller('TwoPlayerCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('initializes', function () {
-  });
-});
-
-
-describe('Controller: MultiPlayerCtrl', function () {
-  // load the controller's module
-  beforeEach(module('twistedHangmanApp'));
-
-  var ctrl, scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ctrl = $controller('MultiPlayerCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('initializes', function () {
-  });
-});
