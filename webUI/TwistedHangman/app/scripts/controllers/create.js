a429afbe6ce1d8d530bff00ed3fb2d7dd3bc9c7d
JTB <jtbdevelopment@gmail.com>
2014-11-22 21:36:26 -0500

    create options

--- webUI/TwistedHangman/app/scripts/controllers/create.js
+++ webUI/TwistedHangman/app/scripts/controllers/create.js
@@ -3,67 +3,70 @@
 //  TODO - test
 
 
-angular.module('twistedHangmanApp').controller('CreateCtrl', function () {
-});
-
 var baseUrl = '/api/player/';
 var restUrl = '/new';
 
-angular.module('twistedHangmanApp').controller('SinglePlayerCtrl', function ($scope, twGameFeatureService, $window, twCurrentPlayerService, $http) {
-  $scope.puzzleSetter = 'SystemPuzzles';
-  $scope.winners = 'SingleWinner';
-  $scope.playerCount = 'SinglePlayer';
-  $scope.thieving = 'Thieving';
-  $scope.drawGallows = '';
-  $scope.drawFace = '';
-  $scope.gamePace = '';
+angular.module('twistedHangmanApp').controller('CreateCtrl', function ($scope, twGameFeatureService, $window, twCurrentPlayerService, $http) {
+  $scope.url = baseUrl + twCurrentPlayerService.currentID() + restUrl;
   $scope.featureData = {};
   twGameFeatureService.features().then(function (data) {
     $scope.featureData = data;
   });
-  $scope.players = [];
-  $scope.url = baseUrl + twCurrentPlayerService.currentID() + restUrl;
+  $scope.thieving = 'Thieving';
+  $scope.drawGallows = '';
+  $scope.drawFace = '';
+  $scope.gamePace = '';
+
+  $scope.setSinglePlayer = function () {
+    $scope.playerCount = 'SinglePlayer';
+    $scope.players = [];
+    $scope.gamePace = '';
+    $scope.puzzleSetter = 'SystemPuzzles';
+    $scope.winners = 'SingleWinner';
+    $scope.h2hEnabled = false;
+    $scope.alternatingEnabled = false;
+    $scope.allFinishedEnabled = false;
+    $scope.turnBasedEnabled = false;
+  };
+
+  $scope.setTwoPlayers = function () {
+    $scope.playerCount = 'TwoPlayer';
+    if ($scope.players.length > 1) {
+      $scope.players = [];
+    }
+    if ($scope.puzzleSetter === 'AlternatingPuzzleSetter') {
+      $scope.puzzleSetter = 'SystemPuzzles';
+    }
+    $scope.h2hEnabled = true;
+    $scope.alternatingEnabled = false;
+    $scope.allFinishedEnabled = true;
+    $scope.turnBasedEnabled = true;
+  };
+
+  $scope.setThreePlayers = function () {
+    $scope.playerCount = 'ThreePlus';
+    if ($scope.puzzleSetter === '') {
+      $scope.puzzleSetter = 'SystemPuzzles';
+    }
+    $scope.h2hEnabled = false;
+    $scope.alternatingEnabled = true;
+    $scope.allFinishedEnabled = true;
+    $scope.turnBasedEnabled = true;
+  };
 
   $scope.createGame = function () {
-    $window.alert('puzzleSetter=' + $scope.puzzleSetter +
-      ', winners=' + $scope.winners +
-      ', playerCount=' + $scope.playerCount +
-      ', thieving=' + $scope.thieving +
-      ', drawGallows=' + $scope.drawGallows +
-      ', drawFace=' + $scope.drawFace +
-      ', gamePace=' + $scope.gamePace
-    );
     var featureNames = ['puzzleSetter', 'playerCount', 'thieving', 'drawGallows', 'drawFace', 'gamePace'];
     var featureSet = [];
+    var players = [];
     featureNames.forEach(function (name) {
       var data = $scope[name];
       if ((typeof data !== 'undefined') && (data !== '')) {
         featureSet.push(data);
       }
     });
-    var players = [];
-    $window.alert(featureSet);
-    $window.alert(players);
-    $window.alert($scope.url);
-    $window.alert(twCurrentPlayerService.currentID());
     $http.post($scope.url, {'players': players, 'features': featureSet});
   };
-});
 
-angular.module('twistedHangmanApp').controller('TwoPlayerCtrl', function ($scope, twGameFeatureService) {
-  $scope.baseFeatures = ['SinglePlayer, SystemPuzzles, SingleWinner'];
-  $scope.thieving = true;
-  $scope.featureData = {};
-  twGameFeatureService.features().then(function (data) {
-    $scope.featureData = data;
-  });
-});
-
-angular.module('twistedHangmanApp').controller('MultiPlayerCtrl', function ($scope, twGameFeatureService) {
-  $scope.baseFeatures = ['SinglePlayer, SystemPuzzles, SingleWinner'];
-  $scope.thieving = true;
-  $scope.featureData = {};
-  twGameFeatureService.features().then(function (data) {
-    $scope.featureData = data;
-  });
+  //  Initialize
+  $scope.setSinglePlayer();
 });
