75f8dde108527244c3407e5e47148a55ae92967f
JTB <jtbdevelopment@gmail.com>
2014-12-01 20:16:09 -0500

    added code coverage to karma

--- webUI/TwistedHangman/test/karma.conf.js
+++ webUI/TwistedHangman/test/karma.conf.js
@@ -36,6 +36,22 @@ module.exports = function (config) {
     // list of files / patterns to exclude
     exclude: [],
 
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+
     // web server port
     port: 8080,
 
@@ -54,7 +70,8 @@ module.exports = function (config) {
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-coverage'
     ],
 
     // Continuous Integration mode
