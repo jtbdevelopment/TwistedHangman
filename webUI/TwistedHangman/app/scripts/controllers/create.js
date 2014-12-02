ab1ff90e274a2f320b7cee7f45965f56fa93cc46
JTB <jtbdevelopment@gmail.com>
2014-12-01 20:19:45 -0500

    moved propsFilter to it's own file

--- webUI/TwistedHangman/app/scripts/controllers/create.js
+++ webUI/TwistedHangman/app/scripts/controllers/create.js
@@ -5,38 +5,6 @@ var TWO_PLAYERS = 'TwoPlayer';
 var MULTI_PLAYER = 'ThreePlus';
 var SYSTEM_PUZZLES = 'SystemPuzzles';
 
-//  TODO - move
-angular.module('twistedHangmanApp').filter('propsFilter', function () {
-  return function (items, props) {
-    var out = [];
-
-    if (angular.isArray(items)) {
-      items.forEach(function (item) {
-        var itemMatches = false;
-
-        var keys = Object.keys(props);
-        for (var i = 0; i < keys.length; i++) {
-          var prop = keys[i];
-          var text = props[prop].toLowerCase();
-          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-            itemMatches = true;
-            break;
-          }
-        }
-
-        if (itemMatches) {
-          out.push(item);
-        }
-      });
-    } else {
-      // Let the output be the input untouched
-      out = items;
-    }
-
-    return out;
-  };
-});
-
 angular.module('twistedHangmanApp').controller('CreateCtrl', function ($rootScope, $scope, twGameFeatureService, twCurrentPlayerService, $http, $location) {
   $scope.url = twCurrentPlayerService.currentPlayerBaseURL() + '/new';
   $scope.featureData = {};
---
+++ webUI/TwistedHangman/app/scripts/filters/propsFilter.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//
+//  Taken from angular-ui-select multi select plunker demo
+//
+angular.module('twistedHangmanApp').filter('propsFilter', function () {
+  return function (items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      items.forEach(function (item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  };
+});
+
