6cd9341ba9e5901ec9b1142ae7e58a7921f122f5
JTB <jtbdevelopment@gmail.com>
2014-12-13 00:28:03 -0500

    added some animation to phase changes

--- webUI/TwistedHangman/app/scripts/controllers/gamefinders.js
+++ webUI/TwistedHangman/app/scripts/controllers/gamefinders.js
@@ -22,12 +22,13 @@ var phasesAndSymbols = {
 angular.forEach(phasesAndSymbols, function (glyph, phase) {
   var name = phase + 'Games';
   angular.module('twistedHangmanApp').controller(name,
-    ['$scope', '$location', 'twGamePhaseService', 'twGameCache',
-      function ($scope, $location, twGamePhaseService, twGameCache) {
+    ['$scope', '$location', '$animate', '$timeout', 'twGamePhaseService', 'twGameCache',
+      function ($scope, $location, $animate, $timeout, twGamePhaseService, twGameCache) {
         $scope.games = [];
         $scope.style = phase.toLowerCase() + 'Button';
         $scope.glyph = 'glyphicon-' + glyph;
         $scope.label = '';
+        $scope.phase = phase;
         $scope.hideGames = false;
 
         $scope.switchHideGames = function () {
@@ -48,6 +49,21 @@ angular.forEach(phasesAndSymbols, function (glyph, phase) {
           loadGames();
         });
 
+        //  TODO - Test
+        $scope.$on('phaseChangeAlert', function (event, game) {
+          //  Brief timeout to allow event to fully propagate so data is current
+          $timeout(function () {
+            if (game.gamePhase === phase) {
+              var buttonId = '#' + game.id;
+              var prop = angular.element(buttonId);
+              if (angular.isDefined(prop)) {
+                $animate.addClass(prop, 'animated shake').then(function () {
+                  $animate.removeClass(prop, 'animated shake');
+                });
+              }
+            }
+          }, 1);
+        });
       }]);
 });
 
