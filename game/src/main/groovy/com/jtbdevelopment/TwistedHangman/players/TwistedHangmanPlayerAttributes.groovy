70f3ba98e64b4f4e9daa09d58e1a23dddc414d75
JTB <jtbdevelopment@gmail.com>
2015-02-04 06:54:36 -0500

    upped version and fixed attributes loading

--- game/src/main/groovy/com/jtbdevelopment/TwistedHangman/players/TwistedHangmanPlayerAttributes.groovy
+++ game/src/main/groovy/com/jtbdevelopment/TwistedHangman/players/TwistedHangmanPlayerAttributes.groovy
@@ -2,12 +2,14 @@ package com.jtbdevelopment.TwistedHangman.players
 
 import com.jtbdevelopment.games.players.GameSpecificPlayerAttributes
 import groovy.transform.CompileStatic
+import org.springframework.data.mongodb.core.mapping.Document
 
 /**
  * Date: 1/30/15
  * Time: 6:34 PM
  */
 @CompileStatic
+@Document
 class TwistedHangmanPlayerAttributes implements GameSpecificPlayerAttributes {
     public static final int DEFAULT_FREE_GAMES_PER_DAY = 10;
 
