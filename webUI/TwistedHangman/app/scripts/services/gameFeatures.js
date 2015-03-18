82931bcdfad2d887d6de192f6c44d790ab80ce9d
JTB <jtbdevelopment@gmail.com>
2015-03-18 11:38:15 -0400

    clearing some comments

--- webUI/TwistedHangman/app/scripts/services/gameFeatures.js
+++ webUI/TwistedHangman/app/scripts/services/gameFeatures.js
@@ -3,7 +3,6 @@
 angular.module('twistedHangmanApp').factory('twGameFeatureService', ['$http', function ($http) {
   return {
     features: function () {
-      //  TODO - local cookie?
       return $http.get('/api/features', {cache: true}).then(function (response) {
         return response.data;
       });
--- webUI/TwistedHangman/app/scripts/services/gamePhases.js
+++ webUI/TwistedHangman/app/scripts/services/gamePhases.js
@@ -3,7 +3,6 @@
 angular.module('twistedHangmanApp').factory('twGamePhaseService', ['$http', function ($http) {
   return {
     phases: function () {
-      //  TODO - local cookie?
       return $http.get('/api/phases', {cache: true}).then(function (response) {
         return response.data;
       });
