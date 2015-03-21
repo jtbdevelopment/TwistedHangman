a49ce9eca84ba7a43c3a21cd680bec6314d8cf81
JTB Development <jtbdevelopment@gmail.com>
2015-03-21 19:08:24 -0400

    temp disable ads popup for now

--- webUI/TwistedHangman/app/scripts/services/ads.js
+++ webUI/TwistedHangman/app/scripts/services/ads.js
@@ -1,15 +1,21 @@
 'use strict';
 
 angular.module('twistedHangmanApp').factory('twAds',
-  ['$modal',
-    function ($modal) {
+  ['$modal', '$q',
+    function ($modal, $q) {
       return {
         showAdPopup: function () {
+          var p = $q.defer();
+
+          p.resolve();
+          return {result: p.promise};
+          /*
           return $modal.open({
             templateUrl: 'views/popupAdDialog.html',
             controller: 'PopupAdCtrl',
             size: 'lg'
           });
+           */
         }
       };
     }
