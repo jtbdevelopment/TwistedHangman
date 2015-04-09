dbf07bc3be7a420123e298e8806f455bfe666b82
JTB Development <jtbdevelopment@gmail.com>
2015-04-08 21:39:46 -0400

    comments

--- game/src/main/groovy/com/jtbdevelopment/TwistedHangman/game/factory/IndividualGamesInitializer.groovy
+++ game/src/main/groovy/com/jtbdevelopment/TwistedHangman/game/factory/IndividualGamesInitializer.groovy
@@ -6,6 +6,8 @@ import groovy.transform.CompileStatic
 /**
  * Date: 1/13/15
  * Time: 7:13 AM
+ *
+ * Note - having this outside game initializer seemed to be a mistake and did not move to core
  */
 @CompileStatic
 interface IndividualGamesInitializer {
