f9f28a6895a2d50f543b5ab70896876bab447e24
JTB Development <jtbdevelopment@gmail.com>
2016-10-11 19:42:49 -0400

    getting rid of alerts widget in prep for classifier

--- webUI/TwistedHangman/app/scripts/controllers/main.js
+++ webUI/TwistedHangman/app/scripts/controllers/main.js
@@ -9,13 +9,9 @@
  */
 angular.module('twistedHangmanApp')
     .controller('MainCtrl',
-        ['$rootScope', '$scope', '$location', 'jtbAppLongName', '$timeout', 'jtbGameCache', 'jtbPlayerService', 'twGameDetails', 'twGameAlerts',
-            function ($rootScope, $scope, $location, jtbAppLongName, $timeout, jtbGameCache, jtbPlayerService, twGameDetails, twGameAlerts) {
-                if (1 === 2) {
-                    console.log(JSON.stringify(twGameAlerts));
-                }
+        ['$rootScope', '$scope', '$location', 'jtbAppLongName', '$timeout', 'jtbGameCache', 'jtbPlayerService', 'twGameDetails',
+            function ($rootScope, $scope, $location, jtbAppLongName, $timeout, jtbGameCache, jtbPlayerService, twGameDetails) {
                 $scope.playerGreeting = '';
-                $scope.alerts = [];
                 $scope.createRefreshEnabled = false;
                 $scope.showAdmin = false;
                 $scope.showLogout = false;
@@ -24,19 +20,6 @@ angular.module('twistedHangmanApp')
                 $scope.includeTemplate = 'views/empty.html';
                 $scope.appName = jtbAppLongName;
                 console.log(jtbGameCache);
-
-                function gamePath(id) {
-                    return '/show/' + id;
-                }
-
-                $scope.goToGame = function (index) {
-                    if (index < 0 || index >= $scope.alerts.length) {
-                        return;
-                    }
-                    var game = $scope.alerts[index];
-                    $scope.alerts.splice(index, 1);
-                    $location.path(gamePath(game.id));
-                };
 
                 $scope.refreshGames = function () {
                     $rootScope.$broadcast('refreshGames', '');
@@ -48,7 +31,6 @@ angular.module('twistedHangmanApp')
                 };
 
                 $scope.$on('playerLoaded', function () {
-                    $scope.alerts = [];
                     $scope.currentPlayer = jtbPlayerService.currentPlayer();
                     $scope.playerGreeting = 'Welcome ' + $scope.currentPlayer.displayName;
                     $scope.showAdmin = $scope.currentPlayer.adminUser || $scope.showAdmin;
@@ -57,51 +39,6 @@ angular.module('twistedHangmanApp')
                     $scope.includeTemplate = 'views/sidebar.html';
                 });
 
-                function generateAlert(gameId, alertMessage) {
-                    $timeout(function () {
-                        //  Don't show alerts if game is currently on display
-                        if ($location.path().indexOf(gamePath(gameId)) < 0) {
-                            $scope.alerts.splice(0, 0, {
-                                id: gameId,
-                                message: alertMessage
-                            });
-                        }
-                    }, 1);
-                }
-
-                $scope.$on('roundOverAlert', function (event, game) {
-                    var score = twGameDetails.gameScoreForPlayer(game, jtbPlayerService.currentPlayer().md5);
-                    if (score > 0) {
-                        generateAlert(game.id, 'Round ended and you scored!');
-                    }
-                    else if (score === 0) {
-                        generateAlert(game.id, 'Round ended and you drew.');
-                    } else {
-                        generateAlert(game.id, 'Round ended and you lost!');
-                    }
-                });
-
-                $scope.$on('playAlert', function (event, game) {
-                    if (game.features.indexOf('TurnBased') >= 0) {
-                        generateAlert(game.id, 'It\'s you\'re turn!');
-                    } else {
-                        generateAlert(game.id, 'Time to play!');
-                    }
-                });
-
-                $scope.$on('challengedAlert', function (event, game) {
-                    generateAlert(game.id, 'You\'ve been challenged!');
-                });
-
-                $scope.$on('declinedAlert', function (event, game) {
-                    generateAlert(game.id, 'A challenge was declined!');
-                });
-
-                $scope.$on('quitAlert', function (event, game) {
-                    generateAlert(game.id, 'A game was quit!');
-                });
-
-                $scope.$on('setupAlert', function (event, game) {
-                    generateAlert(game.id, 'You need to enter a puzzle!');
-                });
-            }]);
+            }
+        ]
+    );
--- webUI/TwistedHangman/test/spec/controllers/main.js
+++ webUI/TwistedHangman/test/spec/controllers/main.js
@@ -8,7 +8,6 @@ describe('Controller: MainCtrl', function () {
     var MainCtrl, rootScope, scope, playerService, gameCache, location, timeout, player, gameDetails;
     var longName = 'long name';
     var logoutCalled;
-    var gameAlerts = {};
 
     // Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope, $timeout, $location) {
@@ -35,8 +34,7 @@ describe('Controller: MainCtrl', function () {
             jtbAppLongName: longName,
             jtbPlayerService: playerService,
             jtbGameCache: gameCache,
-            twGameDetails: gameDetails,
-            twGameAlerts: gameAlerts
+            twGameDetails: gameDetails
         });
     }));
 
@@ -51,7 +49,6 @@ describe('Controller: MainCtrl', function () {
         rootScope.$apply();
         expect(scope.currentPlayer).toEqual(player);
         expect(scope.playerGreeting).toEqual('Welcome XYZ');
-        expect(scope.alerts).toEqual([]);
         expect(scope.createRefreshEnabled).toEqual(true);
         expect(scope.showAdmin).toEqual(true);
         expect(scope.showLogout).toEqual(false);
@@ -70,19 +67,16 @@ describe('Controller: MainCtrl', function () {
         rootScope.$broadcast('playerLoaded');
         rootScope.$apply();
         expect(scope.playerGreeting).toEqual('Welcome XYZ');
-        expect(scope.alerts).toEqual([]);
         expect(scope.showAdmin).toEqual(true);
         expect(scope.showLogout).toEqual(false);
         expect(scope.includeTemplate).toEqual('views/sidebar.html');
 
-        scope.alerts.push('x');
         player = {displayName: 'ABC', md5: '6666', adminUser: false, source: 'MANUAL'};
         rootScope.$broadcast('playerLoaded');
         rootScope.$apply();
         expect(scope.currentPlayer).toEqual(player);
         expect(scope.currentPlayer.displayName).toEqual('ABC');
         expect(scope.playerGreeting).toEqual('Welcome ABC');
-        expect(scope.alerts).toEqual([]);
         expect(scope.showAdmin).toEqual(true);
         expect(scope.showLogout).toEqual(true);
         expect(scope.includeTemplate).toEqual('views/sidebar.html');
@@ -98,207 +92,4 @@ describe('Controller: MainCtrl', function () {
         expect(scope.includeTemplate).toEqual('views/empty.html');
     });
 
-    describe('go to game', function () {
-        beforeEach(function () {
-            scope.alerts = [{id: '1'}, {id: '2'}, {id: '3'}];
-            spyOn(location, 'path');
-        });
-
-        it('negative index', function () {
-            scope.goToGame(-1);
-            expect(location.path).not.toHaveBeenCalled();
-            expect(scope.alerts).toEqual([{id: '1'}, {id: '2'}, {id: '3'}]);
-        });
-
-        it('too high index', function () {
-            scope.goToGame(3);
-            expect(location.path).not.toHaveBeenCalled();
-            expect(scope.alerts).toEqual([{id: '1'}, {id: '2'}, {id: '3'}]);
-
-            scope.goToGame(5);
-            expect(location.path).not.toHaveBeenCalled();
-            expect(scope.alerts).toEqual([{id: '1'}, {id: '2'}, {id: '3'}]);
-        });
-
-        it('hit an index', function () {
-            scope.goToGame(1);
-            expect(location.path).toHaveBeenCalledWith('/show/2');
-            expect(scope.alerts).toEqual([{id: '1'}, {id: '3'}]);
-        });
-    });
-    describe('process alert messages', function () {
-        var game, preMessage;
-        beforeEach(function () {
-            rootScope.$broadcast('playerLoaded');
-            rootScope.$apply();
-            game = {id: 'ZZ'};
-            preMessage = {id: 'Z', message: 'initial alert'};
-            scope.alerts = [preMessage];
-        });
-
-        describe('when player is not watching game', function () {
-            beforeEach(function () {
-                spyOn(location, 'path').and.callFake(function () {
-                    return '/show/Z';
-                });
-            });
-
-            it('quitAlert not watching', function () {
-                rootScope.$broadcast('quitAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'A game was quit!'}, preMessage]);
-            });
-
-            it('declinedAlert not watching', function () {
-                rootScope.$broadcast('declinedAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'A challenge was declined!'}, preMessage]);
-            });
-
-            it('challengedAlert not watching', function () {
-                rootScope.$broadcast('challengedAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'You\'ve been challenged!'}, preMessage]);
-            });
-
-            it('setupAlert not watching', function () {
-                rootScope.$broadcast('setupAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'You need to enter a puzzle!'}, preMessage]);
-            });
-
-            it('play live not watching', function () {
-                game.features = ['Other'];
-                rootScope.$broadcast('playAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'Time to play!'}, preMessage]);
-            });
-
-            it('play turn based not watching', function () {
-                game.features = ['TurnBased'];
-                rootScope.$broadcast('playAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'It\'s you\'re turn!'}, preMessage]);
-            });
-
-            it('round over win not watching', function () {
-                angular.extend(gameDetails, {
-                    gameScoreForPlayer: function (g, md5) {
-                        expect(g).toBe(game);
-                        expect(md5).toEqual('1234');
-                        return 1;
-                    }
-                });
-                rootScope.$broadcast('roundOverAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'Round ended and you scored!'}, preMessage]);
-            });
-
-            it('round over draw not watching', function () {
-                angular.extend(gameDetails, {
-                    gameScoreForPlayer: function (g, md5) {
-                        expect(g).toBe(game);
-                        expect(md5).toEqual('1234');
-                        return 0;
-                    }
-                });
-                rootScope.$broadcast('roundOverAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'Round ended and you drew.'}, preMessage]);
-            });
-
-            it('round over lost not watching', function () {
-                angular.extend(gameDetails, {
-                    gameScoreForPlayer: function (g, md5) {
-                        expect(g).toBe(game);
-                        expect(md5).toEqual('1234');
-                        return -2;
-                    }
-                });
-                rootScope.$broadcast('roundOverAlert', game);
-                timeout.flush();
-                expect(scope.alerts).toEqual([{id: 'ZZ', message: 'Round ended and you lost!'}, preMessage]);
-            });
-        });
-
-        describe('when player is watching game', function () {
-            beforeEach(function () {
-                spyOn(location, 'path').and.callFake(function () {
-                    return '/show/ZZ';
-                });
-            });
-
-            afterEach(function () {
-                expect(scope.alerts).toEqual([preMessage]);
-            });
-
-            it('quitAlert not watching', function () {
-                rootScope.$broadcast('quitAlert', game);
-                timeout.flush();
-            });
-
-            it('declinedAlert not watching', function () {
-                rootScope.$broadcast('declinedAlert', game);
-                timeout.flush();
-            });
-
-            it('challengedAlert not watching', function () {
-                rootScope.$broadcast('challengedAlert', game);
-                timeout.flush();
-            });
-
-            it('setupAlert not watching', function () {
-                rootScope.$broadcast('setupAlert', game);
-                timeout.flush();
-            });
-
-            it('play live not watching', function () {
-                game.features = ['Other'];
-                rootScope.$broadcast('playAlert', game);
-                timeout.flush();
-            });
-
-            it('play turn based not watching', function () {
-                game.features = ['TurnBased'];
-                rootScope.$broadcast('playAlert', game);
-                timeout.flush();
-            });
-
-            it('round over win not watching', function () {
-                angular.extend(gameDetails, {
-                    gameScoreForPlayer: function (g, md5) {
-                        expect(g).toBe(game);
-                        expect(md5).toEqual('1234');
-                        return 1;
-                    }
-                });
-                rootScope.$broadcast('roundOverAlert', game);
-                timeout.flush();
-            });
-
-            it('round over draw not watching', function () {
-                angular.extend(gameDetails, {
-                    gameScoreForPlayer: function (g, md5) {
-                        expect(g).toBe(game);
-                        expect(md5).toEqual('1234');
-                        return 0;
-                    }
-                });
-                rootScope.$broadcast('roundOverAlert', game);
-                timeout.flush();
-            });
-
-            it('round over lost not watching', function () {
-                angular.extend(gameDetails, {
-                    gameScoreForPlayer: function (g, md5) {
-                        expect(g).toBe(game);
-                        expect(md5).toEqual('1234');
-                        return -2;
-                    }
-                });
-                rootScope.$broadcast('roundOverAlert', game);
-                timeout.flush();
-            });
-        });
-    });
 });
