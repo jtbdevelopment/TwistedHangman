d61e46331e8f622813c7b7e2d7c66bf708286709
JTB <jtbdevelopment@gmail.com>
2016-10-12 21:43:24 -0400

    lot of progress on revamping side bar

---
+++ webUI/TwistedHangman/app/scripts/controllers/menu.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('twistedHangmanApp').controller('MenuCtrl',
+    ['$scope', 'jtbGameCache', 'jtbGameClassifier', 'twGameDetails',
+        function ($scope, jtbGameCache, jtbGameClassifier, twGameDetails) {
+            var controller = this;
+
+            controller.phases = [];
+            controller.phaseStyles = {};
+            controller.phaseLabels = {};
+            controller.phaseDescriptions = {};
+            controller.phaseCollapsed = {};
+            controller.games = {};
+            controller.descriptions = {};
+
+            controller.phaseGlyphicons = jtbGameClassifier.getIcons();
+            controller.phases = [];
+            angular.forEach(jtbGameClassifier.getClassifications(), function (value) {
+                controller.phases.push(value);
+                controller.phaseLabels[value] = value;
+                controller.phaseDescriptions[value] = value;
+                controller.games[value] = [];
+                controller.phaseCollapsed[value] = false;
+                //  TODO - why multiple replace
+                controller.phaseStyles[value] = value.toLowerCase().replace(' ', '-').replace(' ', '-').replace('.', '');
+            });
+
+            function updateGames() {
+                angular.forEach(controller.phases, function (phase) {
+                    controller.games[phase] = jtbGameCache.getGamesForPhase(phase);
+                    angular.forEach(controller.games[phase], function (game) {
+                        controller.descriptions[game.id] = twGameDetails.shortGameDescription(game);
+                    });
+                });
+            }
+
+            $scope.$on('gameCachesLoaded', function () {
+                updateGames();
+            });
+
+            $scope.$on('gameRemoved', function () {
+                updateGames();
+            });
+
+            $scope.$on('gameAdded', function () {
+                updateGames();
+            });
+
+            $scope.$on('gameUpdated', function () {
+                updateGames();
+            });
+        }
+    ]
+);
