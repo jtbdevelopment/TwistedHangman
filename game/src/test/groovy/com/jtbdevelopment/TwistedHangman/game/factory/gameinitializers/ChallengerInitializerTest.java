f41da5567d147c9f69549a578897a4b858a29dbb
JTB <jtbdevelopment@gmail.com>
2018-06-07 18:52:25 -0400

    groovy -> java conversions complete for  test classes

---
+++ game/src/test/groovy/com/jtbdevelopment/TwistedHangman/game/factory/gameinitializers/ChallengerInitializerTest.java
@@ -0,0 +1,107 @@
+package com.jtbdevelopment.TwistedHangman.game.factory.gameinitializers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import com.jtbdevelopment.TwistedHangman.TwistedHangmanTestCase;
+import com.jtbdevelopment.TwistedHangman.game.state.Game;
+import com.jtbdevelopment.TwistedHangman.game.state.GameFeature;
+import com.jtbdevelopment.TwistedHangman.game.state.IndividualGameState;
+import com.jtbdevelopment.TwistedHangman.players.TwistedHangmanSystemPlayerCreator;
+import com.jtbdevelopment.games.factory.GameInitializer;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.bson.types.ObjectId;
+import org.junit.Test;
+
+/**
+ * Date: 11/5/14 Time: 7:11 PM
+ */
+public class ChallengerInitializerTest extends TwistedHangmanTestCase {
+
+  private ChallengerInitializer initializer = new ChallengerInitializer();
+
+  @Test
+  public void testOrder() {
+    assert GameInitializer.DEFAULT_ORDER == initializer.getOrder();
+  }
+
+  @Test
+  public void testSystemPuzzles() {
+    Map<ObjectId, IndividualGameState> expectedSolvers = new HashMap<>();
+    expectedSolvers.put(PONE.getId(), null);
+    expectedSolvers.put(PTHREE.getId(), null);
+    Game game = new Game();
+    game.getFeatures().add(GameFeature.SystemPuzzles);
+    game.setSolverStates(expectedSolvers);
+    initializer.initializeGame(game);
+
+    assertEquals(TwistedHangmanSystemPlayerCreator.TH_PLAYER.getId(),
+        game.getWordPhraseSetter());
+    assertEquals(expectedSolvers, game.getSolverStates());
+  }
+
+  @Test
+  public void testTwoPlayerSystemPuzzles() {
+    Map<ObjectId, IndividualGameState> expectedSolvers = new HashMap<>();
+    expectedSolvers.put(PONE.getId(), null);
+    expectedSolvers.put(PTHREE.getId(), null);
+    Game game = new Game();
+    game.getFeatures().addAll(Arrays.asList(GameFeature.TwoPlayer, GameFeature.SystemPuzzles));
+    game.setSolverStates(expectedSolvers);
+    initializer.initializeGame(game);
+
+    assertEquals(TwistedHangmanSystemPlayerCreator.TH_PLAYER.getId(),
+        game.getWordPhraseSetter());
+    assertEquals(expectedSolvers, game.getSolverStates());
+  }
+
+  @Test
+  public void testAlternatingChallenger() {
+    Map<ObjectId, IndividualGameState> expectedSolvers = new HashMap<>();
+    expectedSolvers.put(PTHREE.getId(), null);
+    expectedSolvers.put(PONE.getId(), null);
+
+    Game game = new Game();
+    game.getFeatures().addAll(Arrays.asList(GameFeature.AlternatingPuzzleSetter));
+    game.setSolverStates(expectedSolvers);
+    game.setPlayers(Arrays.asList(PONE, PTHREE));
+    initializer.initializeGame(game);
+
+    assertEquals(PONE.getId(), game.getWordPhraseSetter());
+    assertEquals(expectedSolvers, game.getSolverStates());
+  }
+
+  @Test
+  public void testTwoPlayerAlternatingChallenger() {
+    Map<ObjectId, IndividualGameState> expectedSolvers = new HashMap<>();
+    expectedSolvers.put(PONE.getId(), null);
+    expectedSolvers.put(PTHREE.getId(), null);
+
+    Game game = new Game();
+    game.getFeatures()
+        .addAll(Arrays.asList(GameFeature.AlternatingPuzzleSetter, GameFeature.TwoPlayer));
+    game.setSolverStates(expectedSolvers);
+    game.setPlayers(Arrays.asList((PONE), (PTHREE)));
+    initializer.initializeGame(game);
+
+    assertEquals(PONE.getId(), game.getWordPhraseSetter());
+    assertEquals(expectedSolvers, game.getSolverStates());
+  }
+
+  @Test
+  public void testTwoPlayerSimulataneous() {
+    Map<ObjectId, IndividualGameState> expectedSolvers = new HashMap<>();
+    expectedSolvers.put(PONE.getId(), null);
+    expectedSolvers.put(PTHREE.getId(), null);
+
+    Game game = new Game();
+    game.getFeatures().addAll(Arrays.asList(GameFeature.TwoPlayer));
+    game.setSolverStates(expectedSolvers);
+    initializer.initializeGame(game);
+
+    assertNull(game.getWordPhraseSetter());
+    assertEquals(expectedSolvers, game.getSolverStates());
+  }
+}
---
+++ game/src/test/groovy/com/jtbdevelopment/TwistedHangman/game/factory/gameinitializers/MaxPenaltiesInitializerTest.java
@@ -0,0 +1,89 @@
+package com.jtbdevelopment.TwistedHangman.game.factory.gameinitializers;
+
+import static org.junit.Assert.assertEquals;
+
+import com.jtbdevelopment.TwistedHangman.game.state.Game;
+import com.jtbdevelopment.TwistedHangman.game.state.GameFeature;
+import com.jtbdevelopment.TwistedHangman.game.state.IndividualGameState;
+import com.jtbdevelopment.games.factory.GameInitializer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.bson.types.ObjectId;
+import org.junit.Test;
+
+/**
+ * Date: 11/5/2014 Time: 9:51 PM
+ */
+public class MaxPenaltiesInitializerTest {
+
+  private MaxPenaltiesInitializer initializer = new MaxPenaltiesInitializer();
+
+  @Test
+  public void testOrder() {
+    assertEquals(GameInitializer.LATE_ORDER, initializer.getOrder());
+  }
+
+  @Test
+  public void testBaseGame() {
+    Game game = new Game();
+    game.getFeatures().addAll(Collections.singletonList(GameFeature.Thieving));
+    Map<ObjectId, IndividualGameState> map = new HashMap<>();
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    game.setSolverStates(map);
+
+    initializer.initializeGame(game);
+    game.getSolverStates().values().forEach(
+        state -> assertEquals(IndividualGameState.BASE_PENALTIES, state.getMaxPenalties()));
+  }
+
+  @Test
+  public void testDrawBoth() {
+    Game game = new Game();
+    game.getFeatures()
+        .addAll(Arrays.asList(GameFeature.Thieving, GameFeature.DrawFace, GameFeature.DrawGallows));
+    Map<ObjectId, IndividualGameState> map = new HashMap<>();
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    game.setSolverStates(map);
+
+    initializer.initializeGame(game);
+    game.getSolverStates().values().forEach(state -> assertEquals(
+        (IndividualGameState.BASE_PENALTIES + IndividualGameState.FACE_PENALTIES
+            + IndividualGameState.GALLOWS_PENALTIES), state.getMaxPenalties()));
+  }
+
+  @Test
+  public void testDrawGallows() {
+    Game game = new Game();
+    game.getFeatures()
+        .addAll(Arrays.asList(GameFeature.Thieving, GameFeature.DrawGallows));
+    Map<ObjectId, IndividualGameState> map = new HashMap<>();
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    game.setSolverStates(map);
+
+    initializer.initializeGame(game);
+    game.getSolverStates().values().forEach(state -> assertEquals(
+        (IndividualGameState.BASE_PENALTIES + IndividualGameState.GALLOWS_PENALTIES),
+        state.getMaxPenalties()));
+  }
+
+  @Test
+  public void testDrawFace() {
+    Game game = new Game();
+    game.getFeatures()
+        .addAll(Arrays.asList(GameFeature.Thieving, GameFeature.DrawFace));
+    Map<ObjectId, IndividualGameState> map = new HashMap<>();
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    map.put(new ObjectId(), new IndividualGameState(game.getFeatures()));
+    game.setSolverStates(map);
+
+    initializer.initializeGame(game);
+    game.getSolverStates().values().forEach(state -> assertEquals(
+        (IndividualGameState.BASE_PENALTIES + IndividualGameState.FACE_PENALTIES),
+        state.getMaxPenalties()));
+  }
+}
